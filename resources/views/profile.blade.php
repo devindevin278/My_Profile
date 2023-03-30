7523fd90340e11293f22301125c7f4e90d9a56d8
devinwong278 <devinwong278@gmail.com>
2023-03-31 01:35:07 +0700

    day2.2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body>
     <nav id="navbar-example2" class="navbar sticky-top bg-body-tertiary px-3 mb-3">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Devin</a>
         <ul class="nav nav-pills">
           <li class="nav-item">
             <a class="nav-link" href="#profiles">Profile</a>
@@ -55,7 +55,7 @@
             </div>
 
             <div class="crop">
-                <img class="img-fluid " src="{{ asset('asset/profilepic-removebg.png') }}" alt="">
+                <img class="img-fluid" src="{{ asset('asset/profilepic-removebg.png') }}" alt="">
             </div>
         </section>
 
@@ -176,7 +176,28 @@
         </section>
 
         <section id="education">
-
+            <h1 class="center">My Education Background</h1>
+            <div class="educations">
+                <div class="edu_box">
+                    <div class="edu_text">
+                        <h3>Shenzhen Yaohua Experimental School</h3>
+                        <h5>High School | Sep 2017 – Jul 2020</h5>
+                    </div>
+                    <div class="edu_crop">
+                        <img src="{{ asset('asset/yaohua.jpg') }}" alt="">
+                    </div>
+                </div>
+                
+                <div class="edu_box">
+                    <div class="edu_crop">
+                        <img src="{{ asset('asset/gembalabaik.jpg') }}" alt="">
+                    </div>
+                    <div class="edu_text">
+                        <h3>Gembala Baik School</h3>
+                        <h5>Primary & Secondary School | Jul 2014 – Jun 2017</h5>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <section id="org">
@@ -185,6 +206,44 @@
 
         <section id="projects" class="container">
             <h1 class="center">My Projects</h1>
+            <div id="carouselExampleCaptions" class="carousel slide">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>First slide label</h5>
+                      <p>Some representative placeholder content for the first slide.</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>Second slide label</h5>
+                      <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>Third slide label</h5>
+                      <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
             <div class="project_box">
 
             </div>
